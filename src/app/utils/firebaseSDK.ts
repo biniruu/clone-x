482106f3e6e68ad5767bb6a0c9311cb373750d69
biniruu <biniruu@gmail.com>
2023-10-17 01:17:06 +0900

    chore: set prefix of env from REACT_APP to NEXT_PUBLIC

--- src/app/utils/firebaseSDK.ts
+++ src/app/utils/firebaseSDK.ts
@@ -6,12 +6,12 @@ import { getAuth } from 'firebase/auth'
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: process.env.REACT_APP_API_KEY as string,
-  authDomain: process.env.REACT_APP_AUTH_DOMAIN as string,
-  projectId: process.env.REACT_APP_PROJECT_ID as string,
-  storageBucket: process.env.REACT_APP_STORAGE_BUCKET as string,
-  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID as string,
-  appId: process.env.REACT_APP_APP_ID as string,
+  apiKey: process.env.NEXT_PUBLIC_API_KEY as string,
+  authDomain: process.env.NEXT_PUBLIC_AUTH_DOMAIN as string,
+  projectId: process.env.NEXT_PUBLIC_PROJECT_ID as string,
+  storageBucket: process.env.NEXT_PUBLIC_STORAGE_BUCKET as string,
+  messagingSenderId: process.env.NEXT_PUBLIC_MESSAGING_SENDER_ID as string,
+  appId: process.env.NEXT_PUBLIC_APP_ID as string,
 } as const
 
 // Initialize Firebase
