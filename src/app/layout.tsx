3f5fd6d46c3b23b1ef00199d28e968c2894c29e4
biniruu <biniruu@gmail.com>
2023-10-16 23:51:13 +0900

    feat: add gap to body tag

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,6 @@
 import { type Metadata } from 'next'
 import { ReactNode } from 'react'
+
 import './globals.css'
 
 export const metadata: Metadata = {
@@ -10,7 +11,7 @@ export const metadata: Metadata = {
 export default function RootLayout({ children }: { children: ReactNode }) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body className="p-4">{children}</body>
     </html>
   )
 }
