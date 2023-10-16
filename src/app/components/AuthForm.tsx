babe11473681ff5e2cd6980cf9e97bd94cbc6528
biniruu <biniruu@gmail.com>
2023-10-17 01:18:25 +0900

    feat: implement create user with firebase

--- src/app/components/AuthForm.tsx
+++ src/app/components/AuthForm.tsx
@@ -1,9 +1,27 @@
 'use client'
 
+import { createUserWithEmailAndPassword } from 'firebase/auth'
 import { SyntheticEvent } from 'react'
+
+import { Auth } from 'app/utils/firebaseSDK'
+
+interface CreateWithUser {
+  email: string
+  password: string
+}
 
 // eslint-disable-next-line @typescript-eslint/require-await
 function AuthForm() {
+  const createUser = async ({ email, password }: CreateWithUser) => {
+    try {
+      await createUserWithEmailAndPassword(Auth, email, password)
+      // TODO: input 창 초기화
+    } catch (error) {
+      const err = error as Error
+      console.error(err)
+    }
+  }
+
   const handleSubmit = (e: SyntheticEvent) => {
     e.preventDefault()
     const target = e.target as typeof e.target & {
@@ -12,7 +30,7 @@ function AuthForm() {
     }
     const email = target.email.value
     const password = target.password.value
-    console.log(email, password)
+    void createUser({ email, password })
   }
 
   return (
