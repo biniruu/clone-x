89812ef4a38a120b598a6f1e3bf71daa19f3bb44
biniruu <biniruu@gmail.com>
2023-10-18 03:02:30 +0900

    feat: implement social account authentication

--- src/app/auth/page.tsx
+++ src/app/auth/page.tsx
@@ -1,8 +1,35 @@
-import AuthForm from 'components/AuthForm'
+import { AuthError, signInWithPopup } from 'firebase/auth'
+import { FormEvent } from 'react'
 
-// TODO: add social login
+import AuthForm from 'components/AuthForm'
+import { GoogleAuth } from 'utils/authWithSocialAccounts'
+import { auth } from 'utils/firebaseSDK'
+
+const providers = {
+  google: GoogleAuth,
+}
+
 function Auth() {
-  return <AuthForm />
+  const signInWithSocialAccount = async (e: FormEvent<HTMLButtonElement>) => {
+    const target = e.target as HTMLButtonElement
+    const provider = providers[target.name as keyof typeof providers]
+
+    try {
+      await signInWithPopup(auth, provider)
+    } catch (error) {
+      const err = error as AuthError
+      console.error(err.code)
+    }
+  }
+
+  return (
+    <div>
+      <AuthForm />
+      <div>
+        <button name="google" onClick={signInWithSocialAccount}></button>
+      </div>
+    </div>
+  )
 }
 
 export default Auth
