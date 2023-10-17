528384e84d0d4722e8e6dc74a2931c74d97cab94
biniruu <biniruu@gmail.com>
2023-10-18 04:38:19 +0900

    chore: remove compiler option from next.config.js
    because of an unnecessary option for this project

--- next.config.js
+++ next.config.js
@@ -1,8 +1,5 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  compiler: {
-    styledComponents: true, // styled component 활성화
-  },
   // experimental: {
   //   serverActions: true, // App Router에서 server actions 활성화
   // },
