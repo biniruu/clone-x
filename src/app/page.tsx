cfa33e1afd9143965134f6c056e79ad5a100f736
biniruu <biniruu@gmail.com>
2023-10-18 04:17:47 +0900

    feat: add redirection to main page
    when it doesn't have a login state,
    it'll provide a authentication page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,23 @@
+'use client'
+
+import { useRouter } from 'next/navigation'
+import { useLayoutEffect } from 'react'
+import { useRecoilValue } from 'recoil'
+
 import Navigation from '../components/Navigation'
 
+import authStateAtom from 'recoil/authState'
+
 export default function Home() {
+  const router = useRouter()
+
+  const authState = useRecoilValue(authStateAtom)
+
+  // TODO: 메인 화면이 뜨기 전에 redirection 할 수 있는 방법 적용
+  useLayoutEffect(() => {
+    !authState && router.push('/auth')
+  }, [])
+
   return (
     <>
       <aside>
