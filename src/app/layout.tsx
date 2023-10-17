623da425b431a487b3501506f3bb9134e8e7001e
biniruu <biniruu@gmail.com>
2023-10-18 04:13:22 +0900

    feat: add recoil provider

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import { type Metadata } from 'next'
 import { ReactNode } from 'react'
 
+import RecoilStateProvider from 'utils/RecoilStateProvider'
 import './globals.css'
 
 export const metadata: Metadata = {
@@ -11,7 +12,9 @@ export const metadata: Metadata = {
 export default function RootLayout({ children }: { children: ReactNode }) {
   return (
     <html lang="en">
-      <body className="p-4">{children}</body>
+      <body className="p-4">
+        <RecoilStateProvider>{children}</RecoilStateProvider>
+      </body>
     </html>
   )
 }
