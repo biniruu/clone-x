ac98aebe9a4c1ada0c1fcf48315111a1dd61192e
biniruu <biniruu@gmail.com>
2023-10-18 03:50:28 +0900

    feat: add authState recoil state to auth page
    to set the state as is true or false

--- src/app/auth/page.tsx
+++ src/app/auth/page.tsx
@@ -3,8 +3,10 @@
 import { AuthError, signInWithPopup } from 'firebase/auth'
 import { useRouter } from 'next/navigation'
 import { FormEvent } from 'react'
+import { useSetRecoilState } from 'recoil'
 
 import AuthForm from 'components/AuthForm'
+import authStateAtom from 'recoil/authState'
 import { GoogleAuth } from 'utils/authWithSocialAccounts'
 import { auth } from 'utils/firebaseSDK'
 
@@ -15,11 +17,14 @@ const providers = {
 function Auth() {
   const router = useRouter()
 
+  const setAuthState = useSetRecoilState(authStateAtom)
+
   const authWithProvider = async (name: string) => {
     const provider = providers[name as keyof typeof providers]
 
     try {
       await signInWithPopup(auth, provider)
+      setAuthState(true)
       router.push('/')
     } catch (error) {
       const err = error as AuthError
